7a950e9167e26d52433218e2a30b3a66d1591347
Soreikhudai <khudaisoreipam@gmail.com>
2023-03-10 14:56:42 +0530

    autp logout applied after 5 mionutes

--- src/App.js
+++ src/App.js
@@ -2,11 +2,33 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import AuthPage from "./pages/AuthPage";
 import HomePage from "./pages/HomePage";
 import ProfilePage from "./pages/ProfilePage";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import AuthContext from "./store/auth-context";
 
 const App = () => {
   const authCtx = useContext(AuthContext);
+
+  useEffect(() => {
+    let logoutTimer;
+    const resetLogoutTimer = () => {
+      clearTimeout(logoutTimer);
+      logoutTimer = setTimeout(() => {
+        authCtx.logout(); // Call the logout function from your auth context
+      }, 300000); // Auto logout after 3 minutes of inactivity
+    };
+
+    resetLogoutTimer();
+
+    window.addEventListener("click", resetLogoutTimer);
+    window.addEventListener("mousemove", resetLogoutTimer);
+    window.addEventListener("keydown", resetLogoutTimer);
+
+    return () => {
+      window.removeEventListener("click", resetLogoutTimer);
+      window.removeEventListener("mousemove", resetLogoutTimer);
+      window.removeEventListener("keydown", resetLogoutTimer);
+    };
+  }, [authCtx]);
 
   return (
     <BrowserRouter>
